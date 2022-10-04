427249ca3d0b75dbac955b1175795f76e2ad9a18
edpenha <edithpenha@gmail.com>
2022-10-04 17:12:36 -0300

    service finalizado

--- src/main/java/com/api/services/UserService.java
+++ src/main/java/com/api/services/UserService.java
@@ -2,9 +2,16 @@ package com.api.services;
 
 
 import com.api.domain.User;
+import com.api.domain.dto.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
+    User create(UserDTO obj);
+    User update(UserDTO obj);
+    void delete(Integer id);
 
 }
