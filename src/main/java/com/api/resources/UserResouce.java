903643de62253bd1d55165de368118b833ffa735
edpenha <edithpenha@gmail.com>
2022-10-04 10:52:55 -0300

    Classe use resource

---
+++ src/main/java/com/api/resources/UserResouce.java
@@ -0,0 +1,23 @@
+package com.api.resources;
+
+import com.api.domain.User;
+import com.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResouce {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+}
