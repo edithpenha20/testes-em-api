f852284cd76149c2b9a75b5c263445fbf569daeb
edpenha <edithpenha@gmail.com>
2022-10-05 15:29:59 -0300

    teste para classe UserServiceImpl metodo update com excecao

--- src/test/java/com/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/api/services/impl/UserServiceImplTest.java
@@ -120,7 +120,16 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateAUserThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
     }
 
     @Test
