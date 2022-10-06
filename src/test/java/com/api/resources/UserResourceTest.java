f0105329a17ec3a9add19d5227f8ccd9ddaf09d2
edpenha <edithpenha@gmail.com>
2022-10-06 06:52:23 -0300

    teste para o construtor do UserDTO

--- src/test/java/com/api/resources/UserResourceTest.java
+++ src/test/java/com/api/resources/UserResourceTest.java
@@ -32,8 +32,8 @@ class UserResourceTest {
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado.";
     public static final int INDEX = 0;
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
 
     @InjectMocks
     private UserResource resource;
